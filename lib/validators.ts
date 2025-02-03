252edc32303d5e8e73c68fe2f0f37168a4fb9dff
NHKhanq <nhkhang061203@gmail.com>
2025-02-03 08:52:39 +0700

    fix bug & deployment

--- lib/validators.ts
+++ lib/validators.ts
@@ -13,7 +13,7 @@ export const insertProductSchema = z.object({
     description: z.string().min(3, 'Description must be 3 characters'),
     stock: z.coerce.number(),
     images: z.array(z.string().min(1, 'Product must be 1 image')),
-    isFeatuerd: z.boolean(),
+    isFeatured: z.boolean(),
     banner: z.string().nullable(),
     price: currency
 })
