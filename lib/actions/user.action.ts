4c19799bff9e98483b20ce010add49572d14928b
NHKhanq <nhkhang061203@gmail.com>
2025-02-04 22:25:04 +0700

    sign up scheme & action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,28 +1,70 @@
-'use server';
+"use server";
 
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 //sign in the user with credentials
-export async function signInWithCredentials(preStare:unknown, formData: FormData) {
-    try {
-        const user = signInFormSchema.parse({
-            email: formData.get('email'),
-            password: formData.get('password')
-        })
+export async function signInWithCredentials(
+  preStare: unknown,
+  formData: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
 
-        await signIn('credentials', user)
-        return { success: true, message: 'Signed in Successfully!' }
-    } catch (error) {
-        if(isRedirectError(error)) {
-            throw error
-        }
-        return { success: false, message: 'Invalid email or password' }
-    }   
+    await signIn("credentials", user);
+    return { success: true, message: "Signed in Successfully!" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: "Invalid email or password" };
+  }
 }
 
 //user sign out
 export async function signOutUser() {
-    await signOut();
+  await signOut();
+}
+
+//sign up user
+export async function signUpUser(preState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User sign up successfully!" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+        throw error;
+      }
+      return { success: false, message: "User was not sign up" };
+    }
+  }
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -1,25 +1,41 @@
-import { z } from 'zod'
-import { formatNumberWithDecimal } from './utils'
+import { z } from "zod";
+import { formatNumberWithDecimal } from "./utils";
 
 const currency = z
-.string()
-.refine((value) => /^\d+(2})?$/.test(formatNumberWithDecimal(Number(value))), 'Price must have 2 decimal')
+  .string()
+  .refine(
+    (value) => /^\d+(2})?$/.test(formatNumberWithDecimal(Number(value))),
+    "Price must have 2 decimal"
+  );
 
 export const insertProductSchema = z.object({
-    name: z.string().min(3, 'Name must be 3 characters'),
-    slug: z.string().min(3, 'Slug must be 3 characters'),
-    category: z.string().min(3, 'Category must be 3 characters'),
-    brand: z.string().min(3, 'Brand must be 3 characters'),
-    description: z.string().min(3, 'Description must be 3 characters'),
-    stock: z.coerce.number(),
-    images: z.array(z.string().min(1, 'Product must be 1 image')),
-    isFeatured: z.boolean(),
-    banner: z.string().nullable(),
-    price: currency
-})
+  name: z.string().min(3, "Name must be 3 characters"),
+  slug: z.string().min(3, "Slug must be 3 characters"),
+  category: z.string().min(3, "Category must be 3 characters"),
+  brand: z.string().min(3, "Brand must be 3 characters"),
+  description: z.string().min(3, "Description must be 3 characters"),
+  stock: z.coerce.number(),
+  images: z.array(z.string().min(1, "Product must be 1 image")),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+  price: currency,
+});
 
 // Schema for signing users in
 export const signInFormSchema = z.object({
-    email: z.string().email('Invalid email address'),
-    password: z.string().min(6, 'Password must be 6 characters'),
+  email: z.string().email("Invalid email address"),
+  password: z.string().min(6, "Password must be 6 characters"),
+});
+
+// Schema for signup users in
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be 6 characters"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm Password must be 6 characters"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
   });
