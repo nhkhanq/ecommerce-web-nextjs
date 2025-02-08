2599af2c8315806a6f411887d5fefc23739e4932
nhkhanq <nhkhang061203@gmail.com>
2025-02-08 15:29:07 +0700

    cald price & add to db

--- prisma/migrations/20250208081355_add_cart/migration.sql
+++ prisma/migrations/20250208081355_add_cart/migration.sql
@@ -5,7 +5,7 @@ CREATE TABLE "Cart" (
     "sessionCartId" TEXT NOT NULL,
     "items" JSON[] DEFAULT ARRAY[]::JSON[],
     "itemsPrice" DECIMAL(12,2) NOT NULL,
-    "totalsPrice" DECIMAL(12,2) NOT NULL,
+    "totalPrice" DECIMAL(12,2) NOT NULL,
     "shippingPrice" DECIMAL(12,2) NOT NULL,
     "taxPrice" DECIMAL(12,2) NOT NULL,
     "createdAt" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
